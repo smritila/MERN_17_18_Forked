ac433d5e8b3527e4ebaab6e163898e2e9f74a1a4
Smriti <smrtghoshal24@gmail.com>
2024-12-02 21:43:47 +0530

    chore: update from master

--- Expressjs/SimpleApi/index.js
+++ Expressjs/SimpleApi/index.js
@@ -5,6 +5,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
@@ -12,17 +13,38 @@ app.use(cors());
 app.get("/sachin", (req, res) => {
   res.send("Sachin is the best");
 });
+const students = [
+  { id: 1, name: "Sachin", age: 25 },
+  { id: 2, name: "Rahul", age: 30 },
+  { id: 3, name: "Saurav", age: 35 },
+  { id: 4, name: "Kohli", age: 35 },
+];
+
 // simple api to send student data to html page
 app.get("/students", (req, res) => {
-  const students = [
-    { id: 1, name: "Sachin", age: 25 },
-    { id: 2, name: "Rahul", age: 30 },
-    { id: 3, name: "Saurav", age: 35 },
-    { id: 4, name: "Smriti", age: 36 },
-  ];
   res.json(students);
 });
-
+// write a controller to get single student data
+app.get("/students/:id", (req, res) => {
+  const id = req.params.id;
+  let findStudent = false;
+  let studentDetail = {};
+  for (let student of students) {
+    if (student.id == id) {
+      findStudent = true;
+      studentDetail = student;
+    }
+  }
+  if (findStudent) {
+    res.json(studentDetail);
+  } else {
+    res.json({ message: "Student not found" });
+  }
+});
+app.get("/studentname/:name", (req, res) => {
+  const name = req.params.name;
+  res.json({ message: `Entered name is: ${name}` });
+});
 // start the server
 app.listen(8000, () => {
   console.log("Server is running on port http://localhost:8000");
