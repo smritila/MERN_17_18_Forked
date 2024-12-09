c19e2128ef7f4939ff0258b989f567d1ef2fc07b
Smriti <smrtghoshal24@gmail.com>
2024-12-09 22:04:04 +0530

    feat: implement basic templating using node.js

--- NodeJS/server/index.js
+++ NodeJS/server/index.js
@@ -1,4 +1,8 @@
 const http = require("http");
+const fs = require("fs");
+const path = require("path");
+const url = require("url");
+
 // import mongoose
 const mongoose = require("mongoose");
 
@@ -12,9 +16,26 @@ const connect = async () => {
   }
 };
 // create a server
-const server = http.createServer((request, response) => {
-  response.write("Hello from server");
-  response.end();
+const server = http.createServer((req, res) => {
+  // response.write("Hello from server");
+  // response.end();
+  // Parse the URL and query parameters
+  const parsedUrl = url.parse(req.url, true);
+  const queryParams = parsedUrl.query;
+
+  if (req.method === "GET") {
+    const name = queryParams.name || "Smriti"; // Default to 'Server' if no name is provided
+
+    // Read the index.html file
+    const filePath = path.join(__dirname, "index.html");
+    fs.readFile(filePath, "utf8", (err, data) => {
+      // Replace a placeholder with the name query parameter
+      const updatedContent = data.replace("{{ name }}", name);
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(updatedContent);
+    });
+  }
 });
 
 // listen on port 8000
