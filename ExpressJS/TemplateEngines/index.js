e5356ec1951eab9449961bde9893367acde6d284
Smriti <smrtghoshal24@gmail.com>
2024-12-10 11:46:47 +0530

    chore: dunamic templating.

--- ExpressJS/TemplateEngines/index.js
+++ ExpressJS/TemplateEngines/index.js
@@ -1,3 +1,33 @@
+// const express = require("express");
+// const app = express();
+
+// const port = 8000;
+
+// // configure ejs
+// app.set("view engine", "ejs");
+// // configure views path
+// app.set("views", "./views");
+
+// // configure public folder
+// app.use(express.static("./public"));
+
+// app.get("/", (req, res) => {
+//   const query = req.query;
+//   const name = query.name || query.Name || "Default Name";
+//   const title = query.title || query.Title || "Default Title";
+
+//   res.render("index", {
+//     //title: "EJS intro",
+//     title: "title",
+//     name: "Smritikana",
+//     // name: "Smritikana",
+//   });
+// });
+
+// app.listen(port, () => {
+//   console.log("server running on ", port);
+// });
+
 const express = require("express");
 const app = express();
 
@@ -12,9 +42,16 @@ app.set("views", "./views");
 app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
+  const query = req.query;
+
+  // Read title and name from query params with default values
+  const name = query.name || query.Name || "Default Name";
+  const title = query.title || query.Title || "Default Title";
+
+  // Pass these values to the EJS template
   res.render("index", {
-    title: "EJS intro",
-    name: "Smritikana",
+    title: title,
+    name: name,
   });
 });
 
