9384716cc9e46539cd1378518472df4f133320d0
Smriti <smrtghoshal24@gmail.com>
2024-12-11 15:54:23 +0530

    chore: changes made.

--- TicketBookingSystem/client/src/pages/Places.jsx
+++ TicketBookingSystem/client/src/pages/Places.jsx
@@ -1,12 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../components/Layout";
 import "../../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function Places() {
+  const [placeName, setPlaceName] = useState("");
+  const [placeDescription, setPlaceDescription] = useState("");
+  const [placeAddress, setPlaceAddress] = useState("");
+  const [image, setImage] = useState(null);
+  function handleSubmit(event) {
+    const formObj = { placeName, placeDescription, placeAddress, image };
+    console.log(formObj);
+    event.preventDefault();
+  }
+
   return (
     <Layout>
       <h3 className="display-4 text-center">Create Places</h3>
-      <form className="">
+      <form onSubmit={handleSubmit}>
         <div className="form-group">
           <lable for="">Place Name</lable>
           <input
@@ -14,8 +24,12 @@ function Places() {
             name="placeName"
             id="placeName"
             className="form-control"
+            onChange={(event) => {
+              setPlaceName(event.target.value);
+            }}
           />
-        </div>{" "}
+        </div>
+
         <div className="form-group">
           <label for="description">Description</label>
           <input
@@ -23,6 +37,9 @@ function Places() {
             name="description"
             id="description"
             className="form-control"
+            onChange={(event) => {
+              setPlaceDescription(event.target.value);
+            }}
           />
         </div>
         <div className="form-group">
@@ -32,6 +49,9 @@ function Places() {
             name="placeaddress"
             id="placeaddress"
             className="form-control"
+            onChange={(event) => {
+              setPlaceAddress(event.target.value);
+            }}
           />
         </div>
         <div className="form-group">
@@ -42,7 +62,20 @@ function Places() {
             name="placeimage"
             id="placeimage"
             className="form-control"
+            onChange={(event) => {
+              const file = event.target.files[0];
+              // console.log(event.target.files);
+              if (file) {
+                setImage(file);
+                alert("image uploaded");
+              } else {
+                alert("cannot upload file");
+              }
+            }}
           />
+        </div>
+        <div className="form-group d-flex justify-content-center">
+          <input type="submit" className="btn btn-primary p-3 m-3" />
         </div>
       </form>
     </Layout>
